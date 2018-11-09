df0f3833608b10f22a1ce2f35dd5fe12c9fa6474
AlexSteveChungAlvarez <42816278+AlexSteveChungAlvarez@users.noreply.github.com>
2018-11-09 13:22:44 -0500

    Update Ap3.r

--- Ap3.r
+++ Ap3.r
@@ -1,7 +1,17 @@
 dyn.load("rnEval.so")
 n = 10
 r = 0
+#Tomamos esta función otra vez para comparar
+fn = function(x,y){
+	if(y<x^2){
+		return (1)
+	} else {
+		return (0)
+	}
+}
+
 for(i in 1:n){
 	p = runif(2,-1,1)
-	print(.C("main",p,r)[2])
+	r = c(p,.C("main",p,r)[2],fn(p))
+	print(r)
 }
