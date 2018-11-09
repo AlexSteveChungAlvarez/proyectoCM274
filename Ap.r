25f6dbb322cbe04a0c591beb97fd1d8e6ead2790
AlexSteveChungAlvarez <42816278+AlexSteveChungAlvarez@users.noreply.github.com>
2018-11-09 12:40:16 -0500

    Update Ap.r

--- Ap.r
+++ Ap.r
@@ -1,8 +1,11 @@
-n<-10000
+#
 
+n<-10000 #Número de muestras
 M = c()
 S = c()
 
+#Esta es la función que queremos replicar
+#Tooma un par de coordenadas y devuelve 1 o 0
 fn = function(x,y){
 	if(y<x^2){
 		return (1)
@@ -11,14 +14,17 @@ fn = function(x,y){
 	}
 }
 
+#Se genera una matriz con pares ordenados aleatorios
+#y un vector con sus respectivas salidas
 for(i in 1:n){
 	m = runif(2,-2,2)
 	S = c(S,fn(m[1],m[2]))
 	M = c(M,m)
 }
-dyn.load("rn.so")
-
 M <- matrix(M, nrow = n, byrow = TRUE)
 
-#S <- c(0,1,0,1)
-s<-.C("main",as.integer(2),as.integer(n),t(M),S)[2]
+#se carga el programa en C
+#ejecuta la función "main"
+dyn.load("rn.so")
+s<-.C("main",	as.integer(2),	as.integer(n),	t(M),	S)[2]
+#		2 entradas	n muestras
