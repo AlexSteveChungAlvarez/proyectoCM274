1b25a754655404451c64b3478b4241f21599fc68
AlexSteveChungAlvarez <42816278+AlexSteveChungAlvarez@users.noreply.github.com>
2018-11-09 13:43:29 -0500

    Update Ap3.r

--- Ap3.r
+++ Ap3.r
@@ -12,6 +12,7 @@ fn = function(x,y){
 
 for(i in 1:n){
 	p = runif(2,-1,1)
-	r = c(p,.C("main",p,r)[2],fn(p[1],p[2]))
-	print(r)
+	s = as.numeric(.C("main",p,r)[2])
+	cat("Entrada:\t",p[1],"\t",p[2],"\n")
+	cat("Función original: ",fn(p[1],p[2]),"\tFunción generada",s,"\n\n")
 }
